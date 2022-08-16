0c5346da03bd4132bbcee0343cc0deb63747f8c4
Tushar21021995 <tushartbedkute@gmail.com>
2022-08-16 19:11:00 +0530

    i have done jenkins

---
+++ Dummy/src/main/java/practice/JenkinsTest.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class JenkinsTest 
+{
+	@Test
+	public void JenkinsTest() throws IOException
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		FileInputStream fis = new FileInputStream("./data/CommonProperty.properties");
+		Properties prob = new Properties();
+		prob.load(fis);
+		String Url = prob.getProperty("url");
+		driver.get(Url);
+		System.out.println("Welcome to jenkins");
+		
+	}
+}
+
